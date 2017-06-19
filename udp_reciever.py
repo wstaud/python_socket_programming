c0528741276551d9fe4ca9891878e9607df37d1c
xconstaud <xconstaud@gmail.com>
2017-06-19 18:25:31 -0500

    Update udp sender/reciever and change names

--- udp_reciever.py
+++ udp_reciever.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
@@ -8,4 +9,7 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)
+    # convert json string to dict
+    data = json.loads(data)
     print "recieved message:", data
+    print type(data)
