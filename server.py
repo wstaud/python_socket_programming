b4a97d000847a1ea36236368bf2dfe03bf66c0c4
xconstaud <xconstaud@gmail.com>
2017-06-19 18:04:25 -0500

    init commit

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST = ''
+PORT = 8888
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+
+while 1:
+    data = conn.recv(1024)
+    if not data: break
+    str_reverse = data.split()
+    str_reverse.reverse()
+    data = " ".join(str_reverse)
+    conn.sendall(data)
+conn.close()
